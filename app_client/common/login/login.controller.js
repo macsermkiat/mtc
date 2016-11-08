f778425a4d76a6b3c6965e9339610135995979d1
Mac <macsermkiat@yahoo.com>
2016-11-09 01:28:17 +0700

    Revert "fix previous commit"
    This reverts commit 0822ed2832a8bdf047aac430642e0fb65c1e16ea.

--- app_client/common/login/login.controller.js
+++ app_client/common/login/login.controller.js
@@ -6,21 +6,18 @@
     .module('mtcApp')
     .controller('loginController', loginController);
 
-    loginController.$inject = ['authService', '$translate', '$rootScope', '$scope', '$state', '$timeout', '$window'];
+    loginController.$inject = ['authService', '$translate', '$rootScope', '$scope', '$state', '$timeout'];
 
-    function loginController(authService, $translate, $rootScope, $scope, $state, $timeout, $window) {
+    function loginController(authService, $translate, $rootScope, $scope, $state, $timeout) {
 
       var lgin = this;
       lgin.authService = authService;
       lgin.goLogin = function() {
+        $state.go('/');
+        $timeout(function() {
+          authService.login()
+        },100);
         
-        if ($state.current.name === '/') {
-          authService.login();
-        } else {
-          alert("Please sign in from main page");
-          $state.go('/');
-          $window.location.reload();
-        } 
       };
 
       // i18n
