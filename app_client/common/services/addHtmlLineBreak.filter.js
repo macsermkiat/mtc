4d23b2c40f0dd096fad5c7017e04f554f75a8f0f
Mac <macsermkiat@yahoo.com>
2016-12-27 22:36:02 +0700

    fix bug in filter

--- app_client/common/services/addHtmlLineBreak.filter.js
+++ app_client/common/services/addHtmlLineBreak.filter.js
@@ -6,7 +6,7 @@ angular
 
 	function addHtmlLineBreaks () {
 		return function (text) {
-			var output = text.replace(/\n/g, '<br/>');
+			var output = angular.isDefined(text) ? text.replace(/\n/g, '<br/>') : ''
 			return output;
 		};
 	}
